0d0469d223fe7270b38f67577ae944465f37bc05
santospanda111 <santospanda111@gmail.com>
2021-04-08 12:34:38 +0530

    [santospanda111] added arithmatic.sh

--- arithmatic.sh
+++ arithmatic.sh
@@ -36,3 +36,17 @@ do
 done
 
 echo ${arr[@]}
+
+for (( i = 0; i < ${#arr[@]}; i++ ))
+do
+        for (( j = $i; j < ${#arr[@]}; j++ ))
+        do
+                if [ ${arr[$i]} -gt ${arr[$j]} ]
+                then
+                        temp=${arr[$i]}
+                        arr[$i]=${arr[$j]}
+                        arr[$j]=$temp
+                fi
+        done
+done
+echo ${arr[@]}
